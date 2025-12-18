89a0d624e333b1fcfd6650d7754c26f7e87ca77b
burcu ulgen <burcu.akbulut@ogr.iu.edu.tr>
2025-12-18 18:03:56 +0300

    docs: postgres sink logic added

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,18 +1,22 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-#minio giris
-storage_options = {"key": "minioadmin", "secret": "minioadmin", "client_kwargs": {"endpoint_url": "http://minio:9000"}}
+#
+storage_options = {
+    "key": "minioadmin",
+    "secret": "minioadmin",
+    "client_kwargs": {"endpoint_url": "http://minio:9000"}
+}
+# s3fs kullanarak oku
 df = pd.read_csv("s3://dataops-bronze/raw/dirty_store_transactions.csv", storage_options=storage_options)
 
-#drop
-df = df.drop_duplicates() 
-df = df.dropna()          
-# sayısal olmayanları temizle
-
 #
-engine = create_engine('postgresql://trainuser:trainpassword@postgres:5432/traindb')
+df = df.drop_duplicates()  
+df = df.dropna()           #
+
+
+engine = create_engine('postgresql://admin:admin@postgres:5432/traindb')
+
 df.to_sql('clean_data_transactions', engine, if_exists='replace', index=False)
 
-print("veri temizlendi, postgresql'e yuklendi")
-
+print("veri temizlendi ve DB'ye yazıldı.")
