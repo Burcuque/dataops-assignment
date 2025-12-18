7ba989617a120cf59732b731ab8010c8fc456b42
burcu ulgen <burcu.akbulut@ogr.iu.edu.tr>
2025-12-18 17:57:02 +0300

    feat: cleaning script and postgres sink added

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,12 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 1. MinIO'dan oku (S3 protokolü ile)
-df = pd.read_csv("http://minio:9000/dataops-bronze/raw/dirty_store_transactions.csv")
+#minio giris
+storage_options = {"key": "minioadmin", "secret": "minioadmin", "client_kwargs": {"endpoint_url": "http://minio:9000"}}
+df = pd.read_csv("s3://dataops-bronze/raw/dirty_store_transactions.csv", storage_options=storage_options)
 
-# 2. Temizle
-df = df.drop_duplicates().dropna()
+#drop
+df = df.drop_duplicates() 
+df = df.dropna()          
+# sayısal olmayanları temizle
 
-# 3. PostgreSQL'e yaz
-engine = create_engine('postgresql://user:pass@postgres:5432/traindb')
+#
+engine = create_engine('postgresql://trainuser:trainpassword@postgres:5432/traindb')
 df.to_sql('clean_data_transactions', engine, if_exists='replace', index=False)
+
+print("veri temizlendi, postgresql'e yuklendi")
+
